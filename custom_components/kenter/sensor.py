749b8ca2c6fb93b6244f938c124f3e5b9a4badbd
filmgarage <30559371+filmgarage@users.noreply.github.com>
2025-09-18 08:39:58 +0200

    Update sensor.py
    move channels to separate file
    set long term statistics

--- custom_components/kenter/sensor.py
+++ custom_components/kenter/sensor.py
@@ -1,25 +1,46 @@
-CHANNEL_MAP = {
-    "10180": "consumption",
-    "10280": "feed_in",
-    "11181": "meter_low",
-    "11182": "meter_high",
-    "11281": "feed_in_low",
-    "11282": "feed_in_high",
-    "18280": "billing_feed_in",
-    "18181": "billing_low",
-    "18182": "billing_high",
-    "70180": "gas_supply",
-    "70280": "gas_feed_in",
-    "71180": "gas_meter",
-    "71380": "gas_meter_raw",
-    "60100": "heat_temp_in",
-    "60110": "heat_temp_out",
-    "60180": "heat_energy",
-    "80180": "water_supply",
-    "80280": "water_return",
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .channels import CHANNEL_MAP
+
+DOMAIN = "kenter"
+
+# Map device_class by unit
+DEVICE_CLASS_MAP = {
+    "kWh": "energy",
+    "W": "power",
+    "m³": "gas",
+    "GJ": "energy",  # treat heat energy like electricity energy
+    "°C": "temperature",
 }
 
+# Map state_class by unit
+STATE_CLASS_MAP = {
+    "kWh": "total_increasing",
+    "W": "measurement",
+    "m³": "total_increasing",
+    "GJ": "total_increasing",
+    "°C": "measurement",
+}
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up Kenter sensors based on the config entry."""
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    meters = coordinator.data.get("meters", [])
+
+    entities = []
+    for meter in meters:
+        conn = meter["connectionId"]
+        mpid = meter["meteringPointId"]
+        for ch in meter["channels"]:
+            entities.append(KenterSensor(coordinator, mpid, ch))
+    async_add_entities(entities)
+
+
 class KenterSensor(CoordinatorEntity, SensorEntity):
+    """Representation of a single Kenter channel as a sensor."""
+
     def __init__(self, coordinator, meter_id, channel):
         super().__init__(coordinator)
         self._meter = meter_id
@@ -29,20 +50,30 @@ class KenterSensor(CoordinatorEntity, SensorEntity):
 
     @property
     def name(self):
+        """Return the friendly name of the sensor."""
         return f"Kenter {self._meter} {self._shortname}"
 
     @property
     def unique_id(self):
+        """Return a unique ID for this sensor."""
         return f"{self._meter}_{self._shortname}"
 
     @property
-    def entity_id(self):
-        return f"sensor.kenter_{self._meter}_{self._shortname}"
-
-    @property
     def state(self):
+        """Return the current value of the sensor."""
         return self.coordinator.data.get("values", {}).get(f"{self._meter}_{self._ch}")
 
     @property
     def unit_of_measurement(self):
+        """Return the unit of measurement."""
         return self._unit
+
+    @property
+    def device_class(self):
+        """Return the device class based on unit."""
+        return DEVICE_CLASS_MAP.get(self._unit)
+
+    @property
+    def state_class(self):
+        """Return the state class for long-term statistics."""
+        return STATE_CLASS_MAP.get(self._unit, "measurement")
